7d69c0f1336b606f0f453e48220409a7c50fe330
janasalem89 <janasalem89@gmail.com>
2022-03-12 20:28:40 +0300

    first commit

---
+++ maxVol.cpp
@@ -0,0 +1,32 @@
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+int main()
+{
+   int length;
+   int width;
+   
+   cout<<"enter length:";
+   cin>>length;
+   cout<<"enter width:";
+   cin>>width;
+  
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
